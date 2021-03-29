0b187946687aca481e0d866ed8fa7b5327985cdf
Yash Lalit <yashlalit.23@gmail.com>
2021-03-29 19:53:26 +0530

    Added App Router

---
+++ lib/ui/routes/app_router.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:timeliner/ui/screens/settings.dart';
+import 'package:timeliner/ui/widgets/basic/bottom_navigator.dart';
+
+class AppRouter {
+  Route onGenerateRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case '/':
+        return MaterialPageRoute(
+          builder: (_) => BottomNavigator(),
+        );
+      case '/home':
+        return null;
+
+      case '/feed':
+        return null;
+
+      case '/library':
+        return null;
+
+      case '/important':
+        return null;
+
+      case '/settings':
+        return MaterialPageRoute(
+          builder: (_) => SettingsScreen(),
+        );
+
+      default:
+        return null;
+    }
+  }
+}

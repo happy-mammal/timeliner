31f7f9c651a575a64d2ad244a321bb7057667370
Yash Lalit <yashlalit.23@gmail.com>
2021-03-29 19:47:35 +0530

    Code refractored.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:timeliner/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(TimeLiner());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);

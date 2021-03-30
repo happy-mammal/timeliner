536d94959e4b039f555eebf2ba44eefcf770cc03
Yash Lalit <yashlalit.23@gmail.com>
2021-03-30 16:26:40 +0530

    Code refractored.

--- lib/utils/app_strings.dart
+++ lib/utils/app_strings.dart
@@ -3,10 +3,10 @@ import 'package:google_fonts/google_fonts.dart';
 
 class AppStrings {
   List bnbNames = <Widget>[
-    Text('Home', style: GoogleFonts.roboto()),
-    Text('Search', style: GoogleFonts.roboto()),
-    Text('Library', style: GoogleFonts.roboto()),
-    Text('Pinned', style: GoogleFonts.roboto()),
+    Text('Home', style: GoogleFonts.roboto(fontSize: 16)),
+    Text('Search', style: GoogleFonts.roboto(fontSize: 16)),
+    Text('Intrests', style: GoogleFonts.roboto(fontSize: 16)),
+    Text('Pinned', style: GoogleFonts.roboto(fontSize: 16)),
   ];
 
   String profileUrl = 'https://e3.365dm.com/21/02/2048x1152/skynews-elon-musk_5257586.jpg';
@@ -14,6 +14,8 @@ class AppStrings {
   List imageUrls = <String>[
     'https://starwalk.space/gallery/images/may-2020-starlink-satellites/1920x1080',
   ];
-  List homeNames = <String>["Stories", "Top Trending", "Suggested Feed", "Curated For You"];
-  List searchNames = <String>["Search", "Top Trending", "Suggested Feed", "Curated For You"];
+  List homeNames = <String>["Stories", "Top Trending", "Suggested Feed"];
+  List searchNames = <String>["Search", "Latest News", "Results for "];
+  List intrestNames = <String>["Intrests", "Latest News", "Results for "];
+  List pinnedNames = <String>["Pinned Intrest"];
 }

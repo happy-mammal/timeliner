d6c659a06f3c75b4725d5b2b80947291c3ee4010
Yash Lalit <yashlalit.23@gmail.com>
2021-04-30 16:49:54 +0530

    Code refractored.(Added new routes/bloc providers)

--- lib/presentation/routes/app_route.dart
+++ lib/presentation/routes/app_route.dart
@@ -2,16 +2,23 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:timeliner/presentation/pages/auth.dart';
 import 'package:timeliner/presentation/pages/home.dart';
+import 'package:timeliner/presentation/pages/setup.dart';
 
 class AppRoute {
   Route<dynamic> onGenerateRoute(RouteSettings routeSettings) {
+    final args = routeSettings.arguments;
+
     switch (routeSettings.name) {
       case '/':
         return MaterialPageRoute(builder: (_) => AuthPage());
         break;
-      case '/home':
-        return MaterialPageRoute(builder: (_) => HomeScreen());
+      case '/setup':
+        return MaterialPageRoute(builder: (_) => SetupPage(data: args));
         break;
+      case '/home':
+        return MaterialPageRoute(builder: (_) => HomePage(data: args));
+        break;
+
       default:
         return null;
         break;
